fb5a141ee2c448783f441e753df937b8f5852b92
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-01 15:05:39 +0100

    first commit

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import HandshakeIcon from '@mui/icons-material/Handshake';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import '../Styles/Services.css'
 
 function Services() {
   return (
@@ -9,7 +10,7 @@ function Services() {
     <h1> our service</h1>
     <div className="flex-content">
       <div>
-        <AddCommentIcon/>
+        <div className="icons"><AddCommentIcon/></div>
         <h2> Professional field marketing</h2>
         <hr/>
         <p>
@@ -19,8 +20,8 @@ function Services() {
       </div>
       
       <div>
-        <HandshakeIcon />
-        <h2> Professional field marketing</h2>
+        <div className='icons'><HandshakeIcon /></div>
+        <h2> Customer acquisition campaigns</h2>
         <hr/>
         <p>
         We are experts at setting up, managing and analysing field marketing and acquisition campaigns. Our focus is always on delivering well-informed, happy and committed customers and donors who support our clients’ business and causes over the long term. We’re incredibly proud to have been one of the UK’s preferred field marketing agencies since 1989.
@@ -29,8 +30,8 @@ function Services() {
       </div>
 
       <div>
-      <FavoriteBorderIcon/>
-      <h2> Professional field marketing</h2>
+      <div className='icons'><FavoriteBorderIcon/></div>
+      <h2>Fundraising & donor acquisition</h2>
       <hr/>
       <p>
       We are experts at setting up, managing and analysing field marketing and acquisition campaigns. Our focus is always on delivering well-informed, happy and committed customers and donors who support our clients’ business and causes over the long term. We’re incredibly proud to have been one of the UK’s preferred field marketing agencies since 1989.
